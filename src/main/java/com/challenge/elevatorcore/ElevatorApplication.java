ea60053ac9085a4e54fdb17be096b7c54ab76c80
santiago <santiagorrosa2@gmail.com>
2023-08-17 14:24:10 -0300

    init commit

---
+++ src/main/java/com/challenge/elevatorcore/ElevatorApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.elevatorcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElevatorApplication.class, args);
+	}
+
+}
